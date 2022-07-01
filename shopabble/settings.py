811f98354a5cbab5643606a3958d9072114c688c
= <=>
2022-07-01 15:33:44 +0530

    commit 5

--- shopabble/settings.py
+++ shopabble/settings.py
@@ -26,7 +26,7 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["shoppable-shoping.herokuapp.com","localhost"]
 
