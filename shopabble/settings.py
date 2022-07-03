c4c21142d71ff99a9fb5dc192da7eb7a20734296
= <=>
2022-07-03 15:50:47 +0530

    Revert "Commit 2"
    This reverts commit fe46c510fd215b37380a0d04f04339f6013a6008.

--- shopabble/settings.py
+++ shopabble/settings.py
@@ -23,12 +23,12 @@ STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = 'django-insecure-^&pr$#!q&g&97*)x2!4a+74#drbpvi-hw9#2&ma1_kd0(l(j0y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["shoppable-shoping.herokuapp.com","localhost"]
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -50,7 +50,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -146,7 +145,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
 
 
